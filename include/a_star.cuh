15c4c55cb8e60400b1bb992e9eb1d1699a7bff4b
Mateusz Warzyński <mathew.warzynski@gmail.com>
2019-04-12 01:38:31 +0200

    It compiles!

--- include/a_star.cuh
+++ include/a_star.cuh
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+#define QUEUE_K 2
+#define QUEUE_CAPACITY 10
+
 #define TYPE_PUZZLE 1
 #define TYPE_PATHFINDING 2
+
+static void HandleError(cudaError_t error, const char *file, int line) {
+    if (error != cudaSuccess) {
+        printf("%s in %s at line %d\n", cudaGetErrorString(error), file, line);
+        exit( EXIT_FAILURE );
+    }
+}
+
+#define HANDLE_ERROR(err) (HandleError(err, __FILE__, __LINE__ ))
 
 typedef struct {
     double g;
@@ -15,10 +27,10 @@ typedef struct {
     int size;
 } Map;
 
-int map_hash(Map* H, int j, Node *node);
+__device__ int map_hash(Map* H, int j, Node *node);
 
-Map* map_init(int d, int size);
-void map_deduplicate(Map* H, Node* nodes, Node* nodes_dest, int n);
+__device__ Map* map_init(int d, int size);
+__device__ void map_deduplicate(Map* H, Node* nodes, Node* nodes_dest, int n);
 
 typedef struct Queue {
     int capacity;
@@ -26,8 +38,8 @@ typedef struct Queue {
     Node* items;
 } Queue;
 
-Queue* queues_init(int k, int capacity);
-void queue_push(Queue* queue, Node* node);
-Node* queue_pop(Queue* queue);
-void queues_free(Queue* queue, int k);
+__device__ Queue* queues_init(int k, int capacity);
+__device__ void queue_push(Queue* queue, Node* node);
+__device__ Node* queue_pop(Queue* queue);
+__device__ void queues_free(Queue* queue, int k);
 
--- src/main.cu
+++ src/main.cu
@@ -1,22 +1,61 @@
 #include "a_star.cuh"
 
-__global__ void gpu_a_star(Queue *queues, Node *source, Node *destination, int k) {
+__device__ void queue_init(Queue* queue, int capacity) {
+    queue->items = (Node*)malloc(sizeof(Node)*capacity);
+    queue->capacity = capacity;
+    queue->items_count = 0;
+}
+
+__device__ Queue* queues_init(int k, int capacity) {
+    Queue* queues;
+    queues = (Queue*)malloc(sizeof(Queue)*k);
+    for (int i = 0; i < k; i++) {
+        queue_init(&queues[i], capacity);
+    }
+    return queues;
+}
+
+__device__ void queue_push(Queue *queue, Node* node) {
     return;
 }
 
-__global__ void gpu_nodes_deduplicate() {
-    return;
+__device__ Node* queue_pop(Queue *queue) {
+    return NULL;
 }
 
-__global__ void gpu_append_expanded_nodes() {
-    return;
+__device__ void queue_free(Queue* queue) {
+    free(queue->items);
+}
+
+__device__ void queues_free(Queue* queues, int k) {
+    for (int i = 0; i < k; i++) {
+        queue_free(&queues[i]);
+    }
+    free(queues);
+}
+
+__device__ int map_hash(Map* H, int j, Node *node) {
+    return 7;
+}
+
+__device__ Map* map_init(int d, int size) {
+    return NULL;
+}
+
+__device__ void map_deduplicate(Map* H, Node* nodes, Node* nodes_dest, int n) {}
+
+__global__ void gpu_astar() {
+    int k = QUEUE_K;
+    int capacity = QUEUE_CAPACITY;
+
+    Queue *queues = NULL;
+    queues = queues_init(k, capacity);
+
+    queues_free(queues, k);
 }
 
 int main() {
-    int k = 2;
-    Queue *dqueues = queues_init(k, 4);
-    gpu_a_star<<<1, 1>>>(dqueues, NULL, NULL, k);
-    queues_free(dqueues, k);
+    gpu_astar<<<1, 1>>>();
     return 0;
 }
 
